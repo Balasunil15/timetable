1d3a718866ec2fb143d39744c74b6981eec89ec1
Hariharan <hariharansekarselvam@gmail.com>
2025-03-28 09:31:29 +0530

    hari

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -502,7 +502,7 @@ class userController extends Controller
             ]);
         }
     }
-<<<<<<< HEAD
+
 
     public function fetchSubjects(Request $request)
     {
@@ -527,6 +527,4 @@ class userController extends Controller
         }
     }
 
-=======
->>>>>>> 2e6635a57e3b52fe2274cc86360f3f6b15b73ee6
 }
