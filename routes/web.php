156a0a20ae74fcd5d94c18a3cae9e544ddd3c7d4
Jayaprakash03042005 <jayajayaprakash331@gmail.com>
2025-03-28 09:53:09 +0530

    comments added

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\userController;
 use App\Http\Controllers\FacultyController;
 use App\Http\Controllers\StudentController;
 
+// Authentication Routes
 Route::get('/login', function () {
     return view('login');
 })->name('login');
@@ -16,8 +17,10 @@ Route::get('/', function () {
 });
 
 // Removed name from POST login route to avoid conflict.
-Route::post('/login', [userController::class, 'login']);
+Route::post('/login', [userController::class, 'login']); // Login POST route
+Route::get('/logout', [userController::class, 'logout'])->name('logout'); // Logout route
 
+// Dashboard Routes
 Route::get('/hoddashboard', function () {
     if (!session()->has('fid')) {
         return redirect()->route('login');
@@ -40,29 +43,32 @@ Route::get('/facultydashboard', function () {
         ->header('Expires', '0');
 })->name('facultydashboard');
 
-Route::get('/logout', [userController::class, 'logout'])->name('logout');
-
-// Added routes for subjects and advisors
+// Subject Management Routes
 Route::get('/subjects', [userController::class, 'subjects'])->name('subjects');
 Route::post('/subjects/store', [userController::class, 'storeCourse'])->name('subjects.store');
 Route::post('/subjects/update', [userController::class, 'updateCourse'])->name('subjects.update');
 Route::post('/subjects/delete', [userController::class, 'deleteCourse'])->name('subjects.delete');
+
+// Advisor Management Routes
 Route::get('/advisors', [userController::class, 'advisors'])->name('advisors');
 Route::post('/advisor/store', [userController::class, 'storeAdvisor'])->name('advisor.store');
 // Added route for updating advisor
 Route::post('/advisor/update', [userController::class, 'updateAdvisor'])->name('advisor.update');
 
+// Advisor Subjects and Students Routes
 Route::get('/advisorsubjects', [userController::class, 'advisorsubjects'])->name('advisorsubjects');
 Route::get('/studentslist', [userController::class, 'studentslist'])->name('studentslist');
 
+// Subject Assignment Routes
 // Added route for assigning subject
 Route::post('/subject/assign', [userController::class, 'assignSubject'])->name('subject.assign');
 Route::post('/subject/remove', [userController::class, 'removeSubject'])->name('subject.remove');
 
+// Data Fetching Routes
 // Updated route to use userController for fetching students
 Route::get('/students/fetch', [userController::class, 'fetchStudents'])->name('students.fetch');
 
-
+// Timetable Route
 Route::get('/timetable', function () {
     return view('facultysubject');
 })->name('timetable');
