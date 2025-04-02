f971cf015b4936f94c9b476a44b7c2e07a5e11a1
Balasunil15 <sunildeva2007@gmail.com>
2025-04-02 15:18:44 +0530

    Revert "faculty timetable created"
    This reverts commit 43a9bc6cc86a3961fd03b4cdea482ab03460d343.

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\userController;
+use App\Http\Controllers\FacultyController;
+use App\Http\Controllers\StudentController;
 
 // Authentication Routes
 Route::get('/login', function () {
@@ -65,46 +66,10 @@ Route::post('/subject/assign', [userController::class, 'assignSubject'])->name('
 Route::post('/subject/remove', [userController::class, 'removeSubject'])->name('subject.remove');
 
 // Data Fetching Routes
-Route::get('/students/fetch', function (Request $request) {
-    $subjectCode = $request->input('subjectcode');
-    $cid = session('cid');
+// Updated route to use userController for fetching students
+Route::get('/students/fetch', [userController::class, 'fetchStudents'])->name('students.fetch');
 
-    // Fetch assigned faculty from the subjects table
-    $assignedFaculty = DB::table('subjects')
-        ->where('subjectcode', $subjectCode)
-        ->where('cid', $cid)
-        ->select('fac1id', 'fac2id', 'fname1', 'fname2')
-        ->first();
-
-    $faculty = [];
-    if ($assignedFaculty) {
-        if ($assignedFaculty->fac1id) {
-            $faculty[] = ['fid' => $assignedFaculty->fac1id, 'name' => $assignedFaculty->fname1];
-        }
-        if ($assignedFaculty->fac2id) {
-            $faculty[] = ['fid' => $assignedFaculty->fac2id, 'name' => $assignedFaculty->fname2];
-        }
-    }
-
-    // Fetch students from the student table
-    $students = DB::table('student')
-        ->where('dept', session('dept'))
-        ->where('Batch', session('batch'))
-        ->where('section', session('sec'))
-        ->where('semester', session('semester'))
-        ->select('uid', 'sname', 'sid')
-        ->get();
-
-    return response()->json([
-        'status' => 'success',
-        'data' => [
-            'assignedFaculty' => $faculty,
-            'students' => $students
-        ]
-    ]);
-})->name('students.fetch');
-
-// Timetable Route
+// Timetable Routes
 Route::get('/timetable', function () {
     if (!session()->has('fid')) {
         return redirect()->route('login');
@@ -116,15 +81,7 @@ Route::get('/timetable', function () {
         ->header('Expires', '0');
 })->name('timetable');
 
-Route::get('/subjectsfetch', [userController::class, 'fetchsubjects'])->name('subjects.fetch');
+Route::get('/timetable/data', [userController::class, 'getTimetableData'])->name('timetable.data');
 
-Route::get('/ftimetable', function () {
-    if (!session()->has('fid')) {
-        return redirect()->route('login');
-    }
-    $response = response()->view('facultytimetable');
-    return $response
-        ->header('Cache-Control', 'no-cache, no-store, must-revalidate')
-        ->header('Pragma', 'no-cache')
-        ->header('Expires', '0');
-})->name('ftimetable');
+Route::get('/subjectsfetch', [userController::class, 'fetchSubjects'])->name('subjects.fetch');
+Route::post('/timetable/map', [userController::class, 'mapTimetable'])->name('timetable.map'); // Add this line
